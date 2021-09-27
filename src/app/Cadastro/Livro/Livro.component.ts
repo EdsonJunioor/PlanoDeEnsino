3c7c9595969c213fdfd70791e33e5af9d0a3b7d2
Edson Junior <edson.junior@itixti.com.br>
2021-09-27 14:38:18 -0300

    Criação dos componentes/telas a desenvolver

---
+++ src/app/Cadastro/Livro/Livro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Livro',
+  templateUrl: './Livro.component.html',
+  styleUrls: ['./Livro.component.css']
+})
+export class LivroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LivroComponent } from './Cadastro/Livro/Livro.component';
+import { PlanoEnsinoComponent } from './Cadastro/PlanoEnsino/PlanoEnsino.component';
+import { UsuarioComponent } from './Cadastro/Usuario/Usuario.component';
+import { LoginComponent } from './Login/Login.component';
+import { MenuComponent } from './Menu/Menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      LoginComponent,
+      MenuComponent,
+      LivroComponent,
+      PlanoEnsinoComponent,
+      UsuarioComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
