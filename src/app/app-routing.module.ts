79be1ed366f8fc2b99974f414690c464fd900e55
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2021-09-30 22:07:23 -0300

    alteracoes
    menu e telas de cadastro (livro, plano de ensino, usuario) startadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,17 @@ import { UsuarioComponent } from './Cadastro/Usuario/Usuario.component';
 import { LoginComponent } from './Login/Login.component';
 import { MenuComponent } from './Menu/Menu.component';
 
-const routes: Routes = [     //Sempre que criado um novo componente, dever ser declarado aqui, para manuzeio de rotas e modules.
-  { path: 'login', component: LoginComponent},
-  { path: 'planoEnsino', component: PlanoEnsinoComponent},
-  { path: 'livro', component: LivroComponent},
-  { path: 'usuario', component: UsuarioComponent},
-  { path: 'menu', component: MenuComponent} 
+const routes: Routes = [
+  //Sempre que criado um novo componente, dever ser declarado aqui, para manuzeio de rotas e modules.
+  { path: 'login', component: LoginComponent },
+  { path: 'plano-de-ensino', component: PlanoEnsinoComponent },
+  { path: 'livro', component: LivroComponent },
+  { path: 'usuario', component: UsuarioComponent },
+  { path: 'menu', component: MenuComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
