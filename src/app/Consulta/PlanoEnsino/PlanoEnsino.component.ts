d7fc19f2986d0234bd5ac61f0d9086ee43fcb7e1
Gallonetti <felipegallonettioliveira@gmail.com>
2021-10-08 20:56:32 -0300

    Adições
    Adicionados os componentes de CONSULTA

---
+++ src/app/Consulta/PlanoEnsino/PlanoEnsino.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-PlanoEnsino',
+  templateUrl: './PlanoEnsino.component.html',
+  styleUrls: ['./PlanoEnsino.component.css']
+})
+export class PlanoEnsinoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/Consulta/Sugestao/Sugestao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Sugestao',
+  templateUrl: './Sugestao.component.html',
+  styleUrls: ['./Sugestao.component.css']
+})
+export class SugestaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
