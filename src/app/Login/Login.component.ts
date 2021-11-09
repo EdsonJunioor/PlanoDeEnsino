617e6bca6437d070abde9cf1b01e2ffe480deb1c
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2021-11-09 20:17:20 -0300

    dicas wesley

--- src/app/Login/Login.component.ts
+++ src/app/Login/Login.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-Login',
   templateUrl: './Login.component.html',
-  styleUrls: ['./Login.component.css']
+  styleUrls: ['./Login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    let getLogado = localStorage.getItem('logado');
+    localStorage.setItem('logado', 'false');
   }
 
+  logar() {
+    localStorage.setItem('logado', 'true');
+  }
+
+  ngOnInit() {}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'PlanoDeEnsino';
+
+  getLogado: any;
+
+  constructor() {
+    this.getLogado = localStorage.getItem('logado');
+  }
 }
