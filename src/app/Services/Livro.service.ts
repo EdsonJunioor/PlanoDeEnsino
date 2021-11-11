85be2f84ed66ce84a246e7524aed301baf298b8a
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2021-11-11 20:40:46 -0300

    Criação Livro - Cadastro

---
+++ src/app/Services/Livro.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.prod';
+import { LivroModel } from '../Models/LivroModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LivroService {
+  url = environment.domain + 'livro';
+
+  constructor(private http: HttpClient) {}
+
+  post(livro: LivroModel) {
+    return this.http.post(`${this.url}`, livro);
+  }
+
+  getAll(): Observable<LivroModel[]> {
+    return this.http.get<LivroModel[]>(`${this.url}`);
+  }
+}
